9e5ee8822fdd9766615c789d9af2d34bfd9b162b
Patcwai <40386129+Patcwai@users.noreply.github.com>
2018-06-19 12:27:57 +0200

    Create program.py

---
+++ program.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+#GPIO SETUP
+channel=21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel,GPIO.IN)
+def callback (channel):
+	if GPIO.input(channel):
+		print ("no water detected")
+		else:
+print("water detected")
+GPIO.add__event_detect(chanel, GPIO.BOTH,bouncetime=300)")
+while true:
+ time.sleep(1)
